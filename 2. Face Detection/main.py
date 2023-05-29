7e94ac8ab518346ad2dc04facdd99d1cc2474302
Shrikii <134912394+Shrikii@users.noreply.github.com>
2023-05-29 12:45:40 +0530

    Create main.py

---
+++ 2. Face Detection/main.py
@@ -0,0 +1,21 @@
+import cv2
+alg="haarcascade_frontalface_default.xml"
+haar_cascade=cv2.CascadeClassifier(alg)
+cam=cv2.VideoCapture(0)
+while True:
+    text="No face detected"
+    print(text)
+    _,img=cam.read()
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    face=haar_cascade.detectMultiScale(gray,1.3,4)
+    for(x,y,w,h) in face:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        text="Face detected"
+        print(text)
+    cv2.putText(img,text,(10,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),2)
+    cv2.imshow("Face Detection",img)
+    key=cv2.waitKey(10)
+    if key == 27:
+        break
+cam.release()
+cv2.destroyAllWindows()
